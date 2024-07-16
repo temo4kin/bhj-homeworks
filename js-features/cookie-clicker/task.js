26e7412ecf089abab65e188d0f5996095e3a3ab2
Temo4kin <temo4kinn@gmail.com>
2024-07-16 22:50:55 +0300

    Доделал задачу с кликером.

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -12,40 +12,4 @@ image.onclick = function() {
     image.width -= 10;
     isBig = true;
   }
-  
 }
-
-
-
-// const image = document.getElementById("cookie");
-// const clickerCounter = document.getElementById("clicker__counter");
-
-// function changeSizeBig (){
-//   image.width = image.width + 10;
-// }
-
-// function changeSizeSmall (){
-//   image.width = image.width - 10;
-// }
-
-// function changeText (){
-//   clickerCounter.textContent = clickerCounter.textContent + 1;
-// }
-
-// image.onclick = function () {
-//   changeSizeBig();
-//   clickerCounter.textContent = clickerCounter.textContent + 1;
-//   changeSizeSmall();
-//   image.onclick = null;
-// }
-
-// image.onclick = function () {
-//   changeSizeSmall();
-//   clickerCounter.textContent += 1;
-//   image.onclick = null;
-// }
-
-// image.ondblclick = function () {
-//   resetSize();
-//   changeText();
-// }
