828417c00fc61ed4101119359efd7d96bf7f01f3
Temo4kin <temo4kinn@gmail.com>
2024-07-16 19:20:52 +0300

    Доделал задачу с кликером.

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,45 +1,51 @@
-// const clickerCounter = document.getElementById("clicker-counter");
+const clickerCounter = document.getElementById("clicker__counter");
+const image = document.getElementById("cookie");
+let isBig = true;
+
+
+image.onclick = function() {
+  clickerCounter.textContent = +clickerCounter.textContent + 1;
+  if (isBig) {
+    image.width += 10;
+    isBig = false;
+  } else {
+    image.width -= 10;
+    isBig = true;
+  }
+  
+}
+
+
+
 // const image = document.getElementById("cookie");
+// const clickerCounter = document.getElementById("clicker__counter");
 
+// function changeSizeBig (){
+//   image.width = image.width + 10;
+// }
 
+// function changeSizeSmall (){
+//   image.width = image.width - 10;
+// }
 
-// image.onclick = function() {
-//   clickerCounter.textContent += 1;
-//   image.width += 10;
+// function changeText (){
+//   clickerCounter.textContent = clickerCounter.textContent + 1;
+// }
+
+// image.onclick = function () {
+//   changeSizeBig();
+//   clickerCounter.textContent = clickerCounter.textContent + 1;
+//   changeSizeSmall();
 //   image.onclick = null;
 // }
 
+// image.onclick = function () {
+//   changeSizeSmall();
+//   clickerCounter.textContent += 1;
+//   image.onclick = null;
+// }
 
-
-const image = document.getElementById("cookie");
-const clickerCounter = document.getElementById("clicker__counter");
-
-function changeSizeBig (){
-  image.width = image.width + 10;
-}
-
-function changeSizeSmall (){
-  image.width = image.width - 10;
-}
-
-function changeText (){
-  clickerCounter.textContent = clickerCounter.textContent + 1;
-}
-
-image.onclick = function () {
-  changeSizeBig();
-  clickerCounter.textContent = clickerCounter.textContent + 1;
-  changeSizeSmall();
-  image.onclick = null;
-}
-
-image.onclick = function () {
-  changeSizeSmall();
-  clickerCounter.textContent += 1;
-  image.onclick = null;
-}
-
-image.ondblclick = function () {
-  resetSize();
-  changeText();
-}
+// image.ondblclick = function () {
+//   resetSize();
+//   changeText();
+// }
