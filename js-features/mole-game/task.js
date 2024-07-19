a1db3e96c4709e50e5279f7e615b8c81023ecc80
Temo4kin <temo4kinn@gmail.com>
2024-07-19 13:31:41 +0300

    Оно не работает - задача с кротами

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,20 +1,30 @@
 // const hole = hole.classList.contains('hole_has-mole');
+
+// получаю ID нужных элементов, отчечающих за счетчики
 const deadMole = document.getElementById("dead");
 const lostMole = document.getElementById("lost");
 
+// циклом прохожу по всем лункам - не уверен, что делаю это правильно, но нумерация в HTML с 1 до 9
 for (let i = 1; i < 10; i++) {
+  // получаю очередную лунку, чтобы проверить ее классы
   const hole = document.getElementById("hole" + i);
+  // обработчик нажатия кнопки мыши на лунке
   hole.onklick = function () {
+    // проверяю нужный класс у лунки
     if (hole.classList.contains("hole_has-mole")) {
+      // увеличиваю счетчик с убитыми кротами на 1, если есть нужный класс
       deadMole.textContent = +deadMole.textContent + 1;
-      if (+deadMole.textContent === 10) {
-        alert("You Win!");
-      }
     } else {
+      // увеличиваю счетчик с промахами, если нужный класс отсутствует в лунке
       lostMole.textContent = +lostMole.textContent + 1;
-      if (+lostMole.textContent === 5) {
-        alert("You Lose!");
-      }
     }
   };
+  // проверяю количество попаданий и вывожу сообщение
+  if (+deadMole.textContent === 10) {
+    alert("You Win!");
+  }
+  // проверяю количество промахов и вывожу сообщение
+  if (+lostMole.textContent === 5) {
+    alert("You Lose!");
+  }
 }
