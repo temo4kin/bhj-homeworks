4d86a0d57a99e791adc3ef0efe34268b04477dbc
Temo4kin <temo4kinn@gmail.com>
2024-08-24 14:28:00 +0300

    Немного подправил второе задание про ротацию рекламы

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -2,7 +2,7 @@ const spans = [...document.querySelectorAll('.rotator__case')];
 
 function rotateSpans(spans) {
     for (let i = 0; i < spans.length; i++) {
-        
+
         if (spans[i].classList.contains('rotator__case_active')) {
             spans[i].classList.remove('rotator__case_active');
             nextSpan = spans[i].nextElementSibling;
@@ -12,15 +12,10 @@ function rotateSpans(spans) {
                 nextSpan.classList.add('rotator__case_active');
                 break;
             }
-            
-            
-            
         }
     }
 }
 
-let interval = setInterval(() => {
+setInterval(() => {
     rotateSpans(spans);
 }, 1000);
-
-setTimeout(() => { clearInterval(interval) }, 10000);
